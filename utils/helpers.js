6b46d1905f4d94d933925d59e20f3affa87b842f
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-03-20 00:07:09 +1030

    feat: Implement capitalize() helper function

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+// Helper function to capitalize strings
+function capitalize(string) {
+  const output = string.replace(/\w\S*/g, (w) =>
+    w.replace(/^\w/, (c) => c.toUpperCase())
+  );
+
+  return output;
+}
+
+module.exports = { capitalize };
